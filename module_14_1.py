39aaa072713af07d15870757511812ffccd34309
CDKNash <cdknash123@gmail.com>
2024-11-22 17:32:17 +0400

    Update module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -17,15 +17,15 @@ balance INTEGER NOT NULL
 cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 
 
-# for i in range(1, 11):
-#     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'user"{i}', f'example{i}@gmail.com', f'{i*10}', str(1000)))
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'user"{i}', f'example{i}@gmail.com', f'{i*10}', str(1000)))
 
 
-# for i in range(1, 11, 2):
-#     cursor.execute("UPDATE Users SET balance= ? WHERE id = ?", ('500', f'{i}'))
-#
-# for i in range(1, 11, 3):
-#     cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
+for i in range(1, 11, 2):
+    cursor.execute("UPDATE Users SET balance= ? WHERE id = ?", ('500', f'{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
 
 cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != 60")
 
